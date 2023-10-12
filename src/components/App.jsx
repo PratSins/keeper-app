7604f8b13889822b2d0b3c61b20cf9c996f80300
PratSins <pratyush2002ps@gmail.com>
2023-10-13 02:49:38 +0530

    props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Note from "./Note.jsx";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
